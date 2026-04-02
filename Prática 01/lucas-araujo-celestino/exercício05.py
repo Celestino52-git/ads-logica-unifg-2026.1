dac10602700df86e3e56c4ea4d74c64ab5c79907
Lucas Araujo <lucascelestinoprofissional@gmail.com>
2026-04-01 22:31:30 -0300

    feat: entrega aula 01 - Lucas Araujo Celestino

--- Prática 01/lucas-araujo-celestino/exercício05.py
+++ Prática 01/lucas-araujo-celestino/exercício05.py
@@ -1,5 +1,5 @@
 tamanho_mb = float(input("digite o tamanho do arquivo (Mb): "))
-velocidade_mbps = float(input("digite a velocidade da internet"))
+velocidade_mbps = float(input("digite a velocidade da internet : "))
 tempo_segundos = tamanho_mb / (velocidade_mbps / 8) 
 minutos = int(tempo_segundos // 60)
 segundos_restantes = int(tempo_segundos % 60)
